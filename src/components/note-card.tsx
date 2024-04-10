23ef58f0e9d96f68849ff4c2d11ba1455cf84391
RuanPerroni <rrplongo@gmail.com>
2024-04-09 21:13:26 -0300

    feat: note card infos

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,13 +1,17 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left outline-none bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, error
-        nesciunt placeat facere animi esse eum architecto rerum eveniet
-        praesentium itaque consequuntur eius odit in ipsam labore molestiae
-        mollitia modi?
-      </p>
+    <button className="rounded-md flex flex-col text-left outline-none bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">
+        {note.date.toISOString()}
+      </span>
+      <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
     </button>
