43f31158406d36695f8901ec4605e5f14de6959a
RuanPerroni <rrplongo@gmail.com>
2024-04-12 17:59:01 -0300

    feat: add local storage to cards

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -15,7 +15,10 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col text-left outline-none bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
